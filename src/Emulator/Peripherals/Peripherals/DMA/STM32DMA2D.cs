e172814fddd6318631222cb01cbeae5e7c621cbc
Benjamin Cabé <kartben@users.noreply.github.com>
2021-01-08 12:31:34 +0100

    remove stopwatch

--- src/Emulator/Peripherals/Peripherals/DMA/STM32DMA2D.cs
+++ src/Emulator/Peripherals/Peripherals/DMA/STM32DMA2D.cs
@@ -257,9 +257,6 @@ namespace Antmicro.Renode.Peripherals.DMA
 
         private void DoTransfer()
         {
-            Stopwatch stopWatch = new Stopwatch();
-            stopWatch.Start();
-
             var foregroundFormat = foregroundColorModeField.Value.ToPixelFormat();
             var outputFormat = outputColorModeField.Value.ToPixelFormat();
 
@@ -394,18 +391,6 @@ namespace Antmicro.Renode.Peripherals.DMA
                     }
                 break;
             }
-
-
-            stopWatch.Stop();
-            // Get the elapsed time as a TimeSpan value.
-            TimeSpan ts = stopWatch.Elapsed;
-
-            // Format and display the TimeSpan value.
-            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
-                ts.Hours, ts.Minutes, ts.Seconds,
-                ts.Milliseconds / 10);
-            Console.WriteLine("RunTime " + elapsedTime);
-
 
             startFlag.Value = false;
             transferCompleteFlag.Value = true;
